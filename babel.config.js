1366eb2faf9b8d27e7279a8751295d6868ed7d97
Nimrod Izraely <nimrodizr@gmail.com>
2020-11-07 23:09:37 +0200

    update linters

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,7 @@ module.exports = (api) => {
         {
           alias: {
             features: './src/features',
+            components: './src/shared/components',
           },
         },
       ],
